3ae7e4460daca6eb25c2d55a2153331213a13e0e
3B032062@gm.student.ncut.edu.tw <3B032062@gm.student.ncut.edu.tw>
2023-11-14 03:21:41 +0800

    設定route 跳轉 route

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 */
-
+/*
 Route::get('/', function () {
     return 'welcome';
 });
+*/
 
+Route::get('r1', function () {
+    return redirect('r2');
+});
 
-
-
-
+Route::get('r2', function () {
+    return view('welcome');
+});
 
 
