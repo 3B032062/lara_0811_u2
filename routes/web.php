edb74f1d88aae6b5c031b951be39fed17c6e3f0f
3B032062 <3B032062@gm.student.ncut.edu.tw>
2024-01-01 22:36:10 +0800

    修改完成

--- routes/web.php
+++ routes/web.php
@@ -12,18 +12,18 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-/*設定Route 回傳 view
+//設定Route 回傳 view
 Route::get('/', function () {
     return view('welcome');
 });
-*/
+
 /*設定Route 回傳 字串
 Route::get('/', function () {
     return 'welcome';
 });
 */
 
-/*設定Route 跳轉Route
+//設定Route 跳轉Route
 Route::get('r1', function () {
     return redirect('r2');
 });
@@ -31,39 +31,39 @@ Route::get('r1', function () {
 Route::get('r2', function () {
     return view('welcome');
 });
-*/
+
 /*修改Route接收參數
 Route::get('hello/{name}', function ($name) {
     return 'Hello, '.$name;
 });
 */
-/*修改參數成選擇性
+//修改參數成選擇性
 Route::get('hello/{name}', function ($name='Everybody') {
     return 'Hello, '.$name;
 }) -> name('hello.index');
-*/
-/*
+
+
 Route::get('dashboard', function () {
     return 'dashboard';
 }) ;
 
 Route::group(['prefix'=> 'admin'],function () {
     Route::get('dashboard',function (){
-        return 'dashboard';
+        return 'admin dashboard';
     });
 }) ;
-*/
-//Route::get('hello'[HomeController::class],'index'>name('home.index'));
+
+Route::get('home',[HomeController::class,'index'])->name('home.index');
 
 /*
 Route::get('home', function () {
     return view('home.index');
 });
 */
-
+/*
 use App\Http\Controllers\HelloController;
-Route::get('home/{name?}',[HelloController::class,'index'])->name('hello.index');
-
+Route::get('home/{name?}',[HomeController::class,'index'])->name('home.index');
+*/
 
 
 
